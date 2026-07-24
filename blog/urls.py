685126914c32143befdd080b2b061c47941b4064
Godwill FOKA <cyberill@nousresearch.com>
2026-07-24 02:40:08 +0000

    Fix : ordre URLs blog (rss avant slug)

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.blog_list, name='blog_list'),
-    path('<slug:slug>/', views.blog_detail, name='blog_detail'),
     path('rss/', views.blog_rss, name='blog_rss'),
+    path('<slug:slug>/', views.blog_detail, name='blog_detail'),
 ]
